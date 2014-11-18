74d507a34068b09efbd95723e9ef9bd2ea7e0bf4
Adrian Szalski <szalski.adrian@gmail.com>
2014-11-18 11:53:39 +0100

    Added background, loaded sounds, changed music format

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, spritesheet, os.path
 from pygame.locals import *
 
 # globals for user interface
@@ -9,12 +9,41 @@ lives = 3
 time = 0.5
 started = False
 
+# helper function for loading images
+# define the current path
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+def load_image(file):
+    "loads an image, prepares it for play"
+    file = os.path.join(main_dir, 'art', file)
+    try:
+        surface = pygame.image.load(file)
+    except pygame.error:
+        raise SystemExit('Could not load image "%s" %s'%(file, pygame.get_error()))
+    return surface.convert()
+	
+def load_sound(file):
+    file = os.path.join(main_dir, 'audio', file)
+    sound = pygame.mixer.Sound(file)
+    return sound
+
+
 def main():
 	# Init everything
 	pygame.init()
 	screen = pygame.display.set_mode((WIDTH, HEIGHT))
 	pygame.display.set_caption('Rice Rocks')
-
+	
+	# Load the sounds
+	soundtrack = load_sound('music.ogg')
+	missile_sound = load_sound('shoot.wav')
+	ship_thrust_sound = load_sound('thrust.wav')
+	explosion_sound = load_sound('explode.wav')
+	
+	# Draw the background
+	background = load_image('nebula_blue.f2014.png')
+	screen.blit(background, (0,0))
+	pygame.display.flip()
+	
 	# Init game objects
 	clock = pygame.time.Clock()
 
